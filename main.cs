0953a0d82c66927330af1c7176979963b6d24c8d
Kase1337 <linkinusf1@gmail.com>
2024-05-20 23:46:11 +0000

    Update code, added my major

--- main.cs
+++ main.cs
@@ -2,6 +2,6 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Hello everyone. My name is Wilkin, and I am a student at HCC. I am currently in my second year of studying Cyber Security");
   }
 }
